21f21bf75e366790995b262ec64036eeb7d9a734
ingword <instigator21@gmail.com>
2014-05-23 22:57:24 +0300

    DC-1 Chapter 6 Initial Commit

---
+++ DummyCalculator/Chapter6/DummyCalculator.dpr
@@ -0,0 +1,26 @@
+program DummyCalculator;
+
+uses
+  Vcl.Forms,
+  MainForm in 'MainForm.pas' {fmMain},
+  GUITestRunner,
+  FirstTest in 'Tests\FirstTest.pas',
+  CalculatorGUITest in 'Tests\GUI\CalculatorGUITest.pas',
+  PlusTest in 'Tests\GUI\PlusTest.pas',
+  OperationTest in 'Tests\GUI\OperationTest.pas',
+  MinusTest in 'Tests\GUI\MinusTest.pas',
+  MulTest in 'Tests\GUI\MulTest.pas',
+  DivTest in 'Tests\GUI\DivTest.pas',
+  RandomPlusTest in 'Tests\GUI\RandomPlusTest.pas',
+  Calculator in 'Logic\Calculator.pas',
+  CalculatorOperationTest in 'Tests\Logic\CalculatorOperationTest.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfmMain, fmMain);
+  GUITestRunner.RunRegisteredTestsModeless;
+  Application.Run;
+end.
