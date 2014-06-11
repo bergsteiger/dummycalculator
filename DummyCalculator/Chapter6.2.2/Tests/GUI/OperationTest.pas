2f532ff46047b25ea63150df351491ffc9dea0a9
ingword <instigator21@gmail.com>
2014-06-11 14:11:35 +0300

    DC-12 ZeroDivide Initial Commit

---
+++ DummyCalculator/Chapter6.2.2/Tests/GUI/OperationTest.pas
@@ -0,0 +1,76 @@
+unit OperationTest;
+
+interface
+
+uses
+  CalculatorGUITest,
+  MainForm
+  ;
+
+type
+  TOperation = (opAdd, opMinus, opMul, opDiv, opDivInt);
+
+  TOperationTest = class(TCalculatorGUITest)
+   protected
+    procedure VisitForm(aForm: TfmMain); override;
+    function  GetOp: TOperation; virtual; abstract;
+    function  GetFirstParam: Single; virtual;
+    function  GetSecondParam: Single; virtual;
+  end;//TOperationTest
+
+implementation
+
+uses
+  TestFrameWork,
+  Calculator,
+  SysUtils
+  ;
+
+function TOperationTest.GetFirstParam: Single;
+begin
+ Result := 10;
+end;
+
+function TOperationTest.GetSecondParam: Single;
+begin
+ Result := 20;
+end;
+
+procedure TOperationTest.VisitForm(aForm: TfmMain);
+var
+ aA, aB : Single;
+begin
+ aA := GetFirstParam;
+ aB := GetSecondParam;
+ aForm.edtFirstArg.Text := FloatToStr(aA);
+ aForm.edtSecondArg.Text := FloatToStr(aB);
+ case GetOp of
+  opAdd:
+  begin
+   aForm.btnAdd.Click;
+   Check((aForm.edtResult.Text) = TCalculator.FloatToStr(aA + aB));
+  end;
+  opMinus:
+  begin
+   aForm.btnMinus.Click;
+   Check((aForm.edtResult.Text) = TCalculator.FloatToStr(aA - aB));
+  end;
+  opMul:
+  begin
+   aForm.btnMul.Click;
+   Check((aForm.edtResult.Text) = TCalculator.FloatToStr(aA * aB));
+  end;
+  opDiv:
+  begin
+   aForm.btnDiv.Click;
+   Check((aForm.edtResult.Text) = TCalculator.FloatToStr(aA / aB));
+  end;
+  opDivInt:
+  begin
+   aForm.btnDivInt.Click;
+   Check((aForm.edtResult.Text) = TCalculator.FloatToStr(Round(aA) div Round(aB)));
+  end;
+ end;//case GetOp
+end;
+
+end.
---
+++ DummyCalculator/Chapter6.2.2/Tests/Logic/CalculatorOperationViaEtalonTest.pas
@@ -0,0 +1,99 @@
+unit CalculatorOperationViaEtalonTest;
+
+interface
+
+uses
+  TestFrameWork,
+  Calculator,
+  Tests.Logger;
+
+ type
+  TCalcOperation = function (const A, B: string): string of object;
+
+  TCalculatorOperationViaEtalonTest = class(TTestCase)
+   private
+    procedure CheckOperation(aLogger: TLogger;
+                             aX1, aX2: string;
+                             anOperation : TCalcOperation);
+   published
+    procedure TestDiv;
+    procedure TestMul;
+    procedure TestAdd;
+    procedure TestSub;
+    procedure TestDivInt;
+  end;//TCalculatorOperationViaEtalonTest
+
+implementation
+
+  uses
+    SysUtils;
+
+
+const
+ cA = '5';
+ cB = '10';
+{ TCalculatorOperationViaEtalonTest }
+procedure TCalculatorOperationViaEtalonTest.CheckOperation(aLogger: TLogger;
+                                                           aX1, aX2: string;
+                                                           anOperation : TCalcOperation);
+begin
+  aLogger.OpenTest(Self);
+  aLogger.ToLog(aX1);
+  aLogger.ToLog(aX2);
+  aLogger.ToLog(anOperation(aX1,aX2));
+  CheckTrue(aLogger.CheckWithEtalon);
+end;
+
+procedure TCalculatorOperationViaEtalonTest.TestDiv;
+var
+  x1, x2 : string;
+begin
+  x1:= cA;
+  x2:= cB;
+
+  CheckOperation(g_Logger, x1, x2, TCalculator.Divide);
+end;
+
+procedure TCalculatorOperationViaEtalonTest.TestDivInt;
+var
+  x1, x2  : string;
+begin
+  x1:= cA;
+  x2:= cB;
+
+  CheckOperation(g_Logger, x1, x2, TCalculator.DivInt);
+end;
+
+procedure TCalculatorOperationViaEtalonTest.TestSub;
+var
+  x1, x2  : string;
+begin
+  x1:= cA;
+  x2:= cB;
+
+  CheckOperation(g_Logger, x1, x2, TCalculator.Sub);
+end;
+
+procedure TCalculatorOperationViaEtalonTest.TestMul;
+var
+  x1, x2  : string;
+begin
+  x1:= cA;
+  x2:= cB;
+
+  CheckOperation(g_Logger, x1, x2, TCalculator.Mul);
+end;
+
+procedure TCalculatorOperationViaEtalonTest.TestAdd;
+var
+  x1, x2  : string;
+begin
+  x1:= cA;
+  x2:= cB;
+
+  CheckOperation(g_Logger, x1, x2, TCalculator.Add);
+end;
+
+initialization
+ TestFramework.RegisterTest(TCalculatorOperationViaEtalonTest.Suite);
+end.
