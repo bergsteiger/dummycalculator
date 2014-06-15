aaf8e45cb768261dd5007567210db6fda49ee005
Alex W. Lulin <lulinalex@gmail.com>
2014-06-15 07:20:45 +0400

    - убираем лишнее.

--- DummyCalculator/Chapter6.3/DummyCalculator.dpr
+++ DummyCalculator/Chapter6.3/DummyCalculator.dpr
@@ -6,18 +6,14 @@ uses
   GUITestRunner,
   FirstTest in 'Tests\FirstTest.pas',
   CalculatorGUITest in 'Tests\GUI\CalculatorGUITest.pas',
-  PlusTest in 'Tests\GUI\PlusTest.pas',
   OperationTest in 'Tests\GUI\OperationTest.pas',
-  MinusTest in 'Tests\GUI\MinusTest.pas',
-  MulTest in 'Tests\GUI\MulTest.pas',
-  DivTest in 'Tests\GUI\DivTest.pas',
-  RandomPlusTest in 'Tests\GUI\RandomPlusTest.pas',
   Calculator in 'Logic\Calculator.pas',
   CalculatorOperationViaEtalonTest in 'Tests\Logic\CalculatorOperationViaEtalonTest.pas',
   Tests.Logger in 'Tests\Utils\Tests.Logger.pas',
   CalculatorOperationViaLogicTest in 'Tests\Logic\CalculatorOperationViaLogicTest.pas',
   CalculatorOperationRandomSequenceTest in 'Tests\Logic\CalculatorOperationRandomSequenceTest.pas',
-  CalculatorOperationViaEtalonBaseTest in 'Tests\Logic\CalculatorOperationViaEtalonBaseTest.pas';
+  CalculatorOperationViaEtalonBaseTest in 'Tests\Logic\CalculatorOperationViaEtalonBaseTest.pas',
+  CalculatorOperationViaLogicBaseTest in 'Tests\Logic\CalculatorOperationViaLogicBaseTest.pas';
 
 {$R *.res}
 begin
