7f4c81022f8f14461a020a51252b73dca14672f5
Alex W. Lulin <lulinalex@gmail.com>
2014-06-15 09:11:45 +0400

    - подключили тест, но не подключили логику.

--- DummyCalculator/Chapter6.2.1/MainForm.pas
+++ DummyCalculator/Chapter6.2.1/MainForm.pas
@@ -15,6 +15,7 @@ type
     Button2: TButton;
     Button3: TButton;
     Button4: TButton;
+    Button5: TButton;
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
     procedure Button3Click(Sender: TObject);
